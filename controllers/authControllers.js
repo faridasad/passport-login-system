d46f06c534e2ed5ec0d762ec14c532c22b6d14aa
faridasad <asadlifaridd@gmail.com>
2023-01-26 21:46:30 +0400

    Get user data after successful login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,41 +3,44 @@ import bcrypt from "bcryptjs";
 import createError from "../error.js";
 import jwt from "jsonwebtoken";
 
-
 export const home_get = (req, res) => {
-  if(req.cookies.access_token != undefined)
-    res.render("home", {name: username});
-  else{
-    res.redirect("/login")
+  if (req.cookies.access_token == undefined) res.redirect("/login");
+
+  const token = req.cookies.access_token;
+
+  try {
+    const decoded = jwt.verify(token, "2030");
+    res.render("home", {
+      username: decoded.user.name,
+      email: decoded.user.email,
+    });
+  } catch (err) {
+    res.send("Authentication failed").redirect("/login");
   }
 };
-
 
 export const login_get = (req, res) => {
   res.render("login");
 };
 
-let username = ""
-
 export const login_post = async (req, res, next) => {
   try {
     const user = await User.findOne({ name: req.body.name });
-    username = req.body.name;
     if (!user) return next(createError(404, "User not found!"));
 
     const isCorrect = await bcrypt.compare(req.body.password, user.password);
     if (!isCorrect) return next(createError(400, "Invalid user credentials!"));
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT);
-    const {password, ...others} = user._doc
+    const token = jwt.sign({ user }, "2030");
+    const { password, ...others } = user._doc;
 
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
-      .status(200)
+      .status(200);
 
-    res.redirect("/home")
+    res.redirect("/home");
   } catch (error) {
     console.log(error);
   }
@@ -53,6 +56,8 @@ export const register_post = async (req, res, next) => {
     const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
+
+    console.log("User created successfully!");
     res.redirect("/login");
   } catch (err) {
     next(err);
@@ -60,6 +65,6 @@ export const register_post = async (req, res, next) => {
 };
 
 export const logout_get = (req, res) => {
-  res.clearCookie("access_token")
-  res.redirect("/login")
+  res.clearCookie("access_token");
+  res.redirect("/login");
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,18 +6,18 @@ import {
   register_get,
   register_post,
   logout_get,
-  home_get
+  home_get,
 } from "../controllers/authControllers.js";
 
 const router = express.Router();
-router.use(express.urlencoded({extended: false}))
-router.use(cookieParser())
+router.use(express.urlencoded({ extended: false }));
+router.use(cookieParser());
 
 router.get("/login", login_get);
 router.post("/login", login_post);
 router.get("/register", register_get);
 router.post("/register", register_post);
 router.get("/logout", logout_get);
-router.get("/home", home_get)
+router.get("/home", home_get);
 
 export default router;
